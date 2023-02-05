a4febcad906dc4f88be84a1f720f8f9297e164d2
Stefano Bichicchi <bichicchi.stefano@gmail.com>
2023-02-05 15:06:02 +0100

    netlify conf

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,10 +8,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-			pages: 'dist',
-			assets: 'dist'
-		})
+		adapter: adapter()
 	}
 };
 
