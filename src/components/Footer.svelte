6291e8fd947c5ffbc2195a229779f898c012dd3c
Stefano Bichicchi <stefano.bichicchi@absontheweb.com>
2023-03-13 15:48:19 +0000

    feat: footer links

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,7 +1,25 @@
 <script>
+	import GithubLogo from 'phosphor-svelte/lib/GithubLogo';
+	import LinkedinLogo from 'phosphor-svelte/lib/LinkedinLogo';
+	import InstagramLogo from 'phosphor-svelte/lib/InstagramLogo';
+	import Coffee from 'phosphor-svelte/lib/Coffee'
 	const year = new Date().getFullYear();
 </script>
 
-<footer class="px-4 flex justify-end items-center dark:text-lime-400">
+<footer class="px-4 flex justify-between items-center dark:text-lime-400">
+	<div class="w-1/4 flex justify-evenly">
+		<a href="https://github.com/Stiv1100001" class=" transition-all hover:rotate-45">
+			<GithubLogo size=25/>
+		</a>
+		<a href="https://www.linkedin.com/in/stefano-bichicchi/" class=" transition-all hover:rotate-45">
+			<LinkedinLogo size=25/>
+		</a>
+		<a href="https://www.instagram.com/ehystiv/" class=" transition-all hover:rotate-45">
+			<InstagramLogo size=25/>
+		</a>
+		<a href="https://ko-fi.com/stivvv" class=" transition-all hover:rotate-45">
+			<Coffee size=25/>
+		</a>
+	</div>
 	<h2 class="text-sm italic font-normal">SB - {year}</h2>
 </footer>
