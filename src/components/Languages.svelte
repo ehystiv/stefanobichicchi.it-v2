557d59393a9dc9f32129bf74ca950bb140f30680
Stefano Bichicchi <bichicchi.stefano@gmail.com>
2023-03-26 12:23:25 +0200

    fix style

--- src/components/Languages.svelte
+++ src/components/Languages.svelte
@@ -2,55 +2,38 @@
 	import Section from './shared/Section.svelte';
 
 	import langAndTools from '../lib/data/langAndTools';
-	
-	const langAndToolsTypes = Array.from(new Set(langAndTools.map((x) => x.type)));
 
+	$: ltSort = langAndTools.sort((a, b) => a.type.localeCompare(b.type));
+
+	// const langAndToolsTypes = Array.from(new Set(langAndTools.map((x) => x.type)));
 </script>
 
 <Section id="languages">
-	<div class="h-full basis-full">
-		<div
-			class="h-full grid grid-cols-2 lg:grid-cols-3 grid-rows-[3rem,1fr,1fr,1fr] lg:grid-rows-[3rem,1fr,1fr] gap-3 dark:text-black"
-		>
-			<h1 class="text-4xl col-span-2 lg:col-span-3 dark:text-white">Linguaggi e strumenti 🧰</h1>
-			{#each langAndToolsTypes as type}
-			<div class="card">
-				<div class="title">{type}</div>
-				<div class="container">
-					{#each langAndTools.filter((x) => x.type === type) as lang}
-							<div class="group flex relative">
-								<em class="{lang.icon} icon colored" />
-								<span class="group-hover:opacity-100 transition-opacity bg-gray-800 px-1 text-sm lg:text-lg text-gray-100 rounded-md absolute left-1/2 
-    -translate-x-1/2 translate-y-full opacity-0">{lang.name}</span>
-							</div>
-					{/each}
+	<div class="h-full basis-full p-5">
+		<h1 class="text-4xl mb-5">Come ti posso aiutare?</h1>
+		<div class="flex flex-wrap justify-center items-center gap-2 h-[90%]">
+			{#each ltSort as lt}
+				<div
+					class="icon aspect-square w-16 md:w-24 lg:w-32 flex justify-center items-center bg-white"
+				>
+					<em class="{lt.icon} colored" />
 				</div>
-			</div>
 			{/each}
-			
 		</div>
+		<!-- <div class="h-full flex flex-wrap dark:text-black">
+			{#each langAndTools as lt}
+				<div class="flex justify-center items-center">
+					<div class=" p-1 aspect-square w-20 z-40">
+						<em class="{lt.icon} icon colored w-full bg-white" />
+					</div>
+				</div>
+			{/each}
+		</div> -->
 	</div>
 </Section>
 
 <style lang="scss" scoped>
-	.card {
-		@apply bg-white h-full rounded-md p-3;
-
-		.title {
-			@apply text-lg mb-3;
-		}
-
-		.container {
-			@apply flex flex-wrap justify-between gap-2;
-
-			.name {
-				display: none;
-			}
-
-			.icon {
-				@apply border rounded-lg p-1 shadow-lg text-4xl lg:text-6xl transition-all hover:scale-125;
-
-			}
-		}
+	.icon {
+		@apply border rounded-lg p-1 shadow-lg text-4xl lg:text-6xl transition-all hover:scale-125;
 	}
 </style>
