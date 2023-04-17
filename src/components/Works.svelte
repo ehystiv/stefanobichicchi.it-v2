8379ba2b219aedb73211d673d99681656e914dce
Stefano Bichicchi <stefano.bichicchi@absontheweb.com>
2023-04-17 10:06:43 +0000

    cheange color

--- src/components/Works.svelte
+++ src/components/Works.svelte
@@ -8,7 +8,7 @@
 
 <Section id="works" to="#languages">
 	<div
-		class="h-full basis-full sm:p-0 lg:p-1 grid sm:grid-cols-1 sm:grid-rows-2 lg:grid-cols-2 lg:grid-rows-1 gap-y-4 lg:gap-x-4 py-4 dark:text-black"
+		class="h-full basis-full sm:p-0 lg:p-1 grid sm:grid-cols-1 sm:grid-rows-2 lg:grid-cols-2 lg:grid-rows-1 gap-y-4 lg:gap-x-4 py-4"
 	>
 		<div class="max-h-full overflow-hidden">
 			<Timeline title="Lavori" items={works} />
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,5 +9,14 @@ module.exports = {
 			}
 		}
 	},
-	plugins: [require('tailwind-scrollbar-hide')]
+	plugins: [
+		require('tailwind-scrollbar-hide'),
+		require('@catppuccin/tailwindcss')({
+			// prefix to use, e.g. `text-pink` becomes `text-ctp-pink`.
+			// default is `false`, which means no prefix
+			prefix: 'ctp',
+			// which flavour of colours to use by default, in the `:root`
+			defaultFlavour: 'mocha'
+		})
+	]
 };
